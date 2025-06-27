b291241eb65a42ad84fded8d2888c6fb0528d776
vanncomiso <vanncomiso@gmail.com>
2025-06-28 02:45:10 +0800

    Add rounded corners to hero section and remove contact section

--- src/SinglePage.jsx
+++ src/SinglePage.jsx
@@ -397,7 +397,7 @@ const SinglePage = () => {
           className="relative z-10 h-dvh w-screen rounded-lg bg-gray-900"
         >
           {/* Static dark background */}
-          <div className="absolute left-0 top-0 size-full bg-gradient-to-br from-gray-900 via-gray-800 to-black"></div>
+          <div className="absolute left-0 top-0 size-full bg-gradient-to-br from-gray-900 via-gray-800 to-black rounded-2xl"></div>
 
           <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-gray-600">
             G<b>A</b>MING
@@ -614,46 +614,6 @@ const SinglePage = () => {
                 containerClass="mt-5 relative z-30"
               />
             </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Contact */}
-      <div id="contact" className="my-20 min-h-96 w-screen px-10">
-        <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
-          <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
-            <ImageClipBox
-              src="/img/contact-1.webp"
-              clipClass="contact-clip-path-1"
-            />
-            <ImageClipBox
-              src="/img/contact-2.webp"
-              clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
-            />
-          </div>
-
-          <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
-            <ImageClipBox
-              src="/img/swordman-partial.webp"
-              clipClass="absolute md:scale-125"
-            />
-            <ImageClipBox
-              src="/img/swordman.webp"
-              clipClass="sword-man-clip-path md:scale-125"
-            />
-          </div>
-
-          <div className="flex flex-col items-center text-center">
-            <p className="mb-10 font-anton text-[10px] uppercase">
-              Join Zentry
-            </p>
-
-            <AnimatedTitle
-              title="let&#39;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether."
-              className="special-font !md:text-[6.2rem] w-full font-anton !text-5xl !font-black !leading-[.9]"
-            />
-
-            <Button title="contact us" containerClass="mt-10 cursor-pointer" />
           </div>
         </div>
       </div>
