024645eaa07ade8d0a5e175cc5689762b669e692
vanncomiso <vanncomiso@gmail.com>
2025-06-28 02:46:53 +0800

    Add rounded corners to hero content overlay

--- src/SinglePage.jsx
+++ src/SinglePage.jsx
@@ -403,7 +403,7 @@ const SinglePage = () => {
             G<b>A</b>MING
           </h1>
 
-          <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="absolute left-0 top-0 z-40 size-full rounded-2xl">
             <div className="mt-24 px-5 sm:px-10">
               <h1 className="special-font hero-heading text-blue-100">
                 redefi<b>n</b>e
